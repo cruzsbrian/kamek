32580d0d10c09f141f70cbcb85a2f7f136584156
Brian Cruz <cruz.s.brian@gmail.com>
2021-05-20 12:46:54 -0400

    cli takes arguments for color

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,14 +1,41 @@
+#include <iostream>
+#include <cstring>
+
 #include "game/game.h"
 #include "game/human_player.h"
 #include "game/cpu.h"
 
 
+#define BLACK false
+#define WHITE true
+
+
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "usage: " << argv[0] << " COLOR\n";
+        exit(1);
+    }
+
+    bool bot_color;
+    if (strcmp(argv[1], "black") == 0 || strcmp(argv[1], "b") == 0) {
+        bot_color = BLACK;
+    } else if (strcmp(argv[1], "white") == 0 || strcmp(argv[1], "w") == 0) {
+        bot_color = WHITE;
+    } else {
+        cerr << "use [black, white, b, w] to specify color\n";
+        exit(1);
+    }
+
     HumanPlayer human;
     CPU cpu{13,22};
 
-    Game game{&human, &cpu};
-    game.play(true);
+    if (bot_color == BLACK) {
+        Game game{&cpu, &human};
+        game.play(true);
+    } else {
+        Game game{&human, &cpu};
+        game.play(true);
+    }
 
     return 0;
 }
