956efb4796017183c30462039b0e22f96748ba0a
Brian Cruz <cruz.s.brian@gmail.com>
2022-01-09 17:28:25 -0800

    Better forward pruning

--- src/alphabeta.cpp
+++ src/alphabeta.cpp
@@ -14,8 +14,9 @@ const int MED_CUTOFF = 5;
 
 const int SORT_DEPTH_REDUCTION = 5;
 
-const int STATIC_EVAL_MARGIN_MEDIUM = 350;
-const int STATIC_EVAL_MARGIN_SHALLOW = 200;
+const int STATIC_EVAL_MARGIN_MEDIUM = 400;
+const int STATIC_EVAL_MARGIN_SHALLOW = 300;
+const int PROBCUT_MARGIN = 100;
 
 
 SearchNode ab_deep(board::Board b, int alpha, int beta, int depth, bool passed, SearchInfo &si) {
@@ -46,6 +47,23 @@ SearchNode ab_deep(board::Board b, int alpha, int beta, int depth, bool passed, 
     // Check for timeout.
     if (get_time_since(si.start) >= si.time_limit) {
         return {depth, NodeType::TIMEOUT, 0, MOVE_NULL};
+    }
+
+    // Probcut
+    if (si.forward_prune) {
+        int prob_depth = max(depth - 2, 0);
+
+        if (alpha != -INT_MAX) {
+            int bound_low = alpha - PROBCUT_MARGIN;
+            SearchNode prob_low = ab_deep(b, bound_low, bound_low + 1, prob_depth, passed, si);
+            if (prob_low.type == NodeType::LOW) return {depth, NodeType::LOW, prob_low.score, MOVE_NULL};
+        }
+
+        if (beta != INT_MAX) {
+            int bound_high = beta + PROBCUT_MARGIN;
+            SearchNode prob_high = ab_deep(b, bound_high - 1, bound_high, prob_depth, passed, si);
+            if (prob_high.type == NodeType::HIGH) return {depth, NodeType::HIGH, prob_high.score, MOVE_NULL};
+        }
     }
 
     int sort_depth = max(0, depth - SORT_DEPTH_REDUCTION);
@@ -84,8 +102,7 @@ SearchNode ab_deep(board::Board b, int alpha, int beta, int depth, bool passed, 
         if (depth <= DEEP_CUTOFF) {
             score = -ab_medium(m.after, -beta, -best_score, depth - 1, false, si);
         } else {
-            SearchNode result = 
-                ab_deep(m.after, -beta, -best_score, depth - 1, false, si);
+            SearchNode result = ab_deep(m.after, -beta, -best_score, depth - 1, false, si);
             if (result.type == NodeType::TIMEOUT) { // propagate timeouts back up
                 return {depth, NodeType::TIMEOUT, 0, MOVE_NULL};
             }
@@ -116,18 +133,25 @@ SearchNode ab_deep(board::Board b, int alpha, int beta, int depth, bool passed, 
 int ab_medium(board::Board b, int alpha, int beta, int depth, bool passed, SearchInfo &si) {
     si.nodes++;
 
-    // Static eval pruning
-    if (si.forward_prune) {
-        int static_score = eval::score(b);
-        if (static_score >= beta + STATIC_EVAL_MARGIN_MEDIUM) {
-            return static_score;
-        } else if (static_score <= alpha - STATIC_EVAL_MARGIN_MEDIUM) {
-            return static_score;
-        }
-    }
-
     if (depth == 0) {
         return eval::score(b);
+    }
+
+    // Probcut
+    if (si.forward_prune) {
+        int prob_depth = max(0, depth - 2);
+
+        if (alpha != -INT_MAX) {
+            int bound_low = alpha - PROBCUT_MARGIN;
+            int prob_low = ab(b, bound_low, bound_low + 1, prob_depth, passed, si);
+            if (prob_low <= bound_low) return prob_low;
+        }
+
+        if (beta != INT_MAX) {
+            int bound_high = beta + PROBCUT_MARGIN;
+            int prob_high = ab(b, bound_high - 1, bound_high, prob_depth, passed, si);
+            if (prob_high >= bound_high) return prob_high;
+        }
     }
 
     int sort_depth = max(0, depth - SORT_DEPTH_REDUCTION);
@@ -157,18 +181,18 @@ int ab_medium(board::Board b, int alpha, int beta, int depth, bool passed, Searc
 int ab(board::Board b, int alpha, int beta, int depth, bool passed, SearchInfo &si) {
     si.nodes++;
 
+    if (depth == 0) {
+        return eval::score(b);
+    }
+
     // Static eval pruning
     if (si.forward_prune) {
         int static_score = eval::score(b);
-        if (static_score > beta + STATIC_EVAL_MARGIN_SHALLOW) {
+        if (static_score - STATIC_EVAL_MARGIN_SHALLOW > beta) {
             return static_score;
-        } else if (static_score < alpha - STATIC_EVAL_MARGIN_SHALLOW) {
+        } else if (static_score + STATIC_EVAL_MARGIN_SHALLOW < alpha) {
             return static_score;
         }
-    }
-
-    if (depth == 0) {
-        return eval::score(b);
     }
 
     uint64_t move_mask = board::get_moves(b);
